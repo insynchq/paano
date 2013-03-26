620f192e460ab7bd4dc1bae3e04daff5d00879f2
Mark Steve Samson <marksteve@insynchq.com>
2013-03-26 16:54:24 +0800

    Bump version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name='paano',
-    version='0.1.7',
+    version='0.1.8',
     url='http://github.com/marksteve/paano',
     license='MIT',
     author='Mark Steve Samson',
