6510df0d1df7043c489347a9657f4b0d9249d90d
Mark Steve Samson <marksteve@insynchq.com>
2013-07-18 10:34:25 +0800

    Change static url path to /paano/static

--- paano/wsgi.py
+++ paano/wsgi.py
@@ -1,7 +1,8 @@
 from flask import Flask
 
 
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__, static_url_path='/paano/static',
+            instance_relative_config=True)
 app.config.from_object('paano.config')
 app.config.from_pyfile('application.cfg', silent=True)
 application = app
